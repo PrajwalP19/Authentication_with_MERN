a21c45bdee56bf32ff627f8f743ed597d74d47df
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-07-19 19:31:46 +0530

    built forgot-password, reset-password, check-auth routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import { connectDB } from "./db/connectDB.js"
 import dotenv from "dotenv"
 import authRoute from "./routes/auth.route.js"
-
+import cookieParser from "cookie-parser"
 
 
 
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 //global middlewares
 
 app.use(express.json())   // allows us to parse incoming requests : req.body
-
+app.use(cookieParser())   //allows us to parse incoming cookies
 
 
 app.use("/api/v1/auth", authRoute)
