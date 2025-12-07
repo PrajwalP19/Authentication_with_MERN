361eeb5eceb855d718680d90dfd2e06d7b94f39b
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-12-07 22:17:30 +0530

    Added one line in feature-login branch

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -4,6 +4,8 @@ const ContactPage = () => {
   return (
     <div>
         <h1>This is a demo contact page, just to learn about how PR is created.</h1>
+
+        <h2>Adding another line, also to practice.</h2>
     </div>
   )
 }
